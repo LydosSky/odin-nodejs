b6c0c7fa44d3caae7274796172e0cf1dccc436c2
Lydos <lydossky@gmail.com>
2025-01-04 21:37:41 +0300

    end of lesson 9

--- ninjanode/app.js
+++ ninjanode/app.js
@@ -2,15 +2,56 @@ const express = require('express');
 const app = express();
 const blogs = require('./blogs');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+const DBURI =
+  'mongodb+srv://admin:nodepassword@cluster0.houd8.mongodb.net/node?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose
+  .connect(DBURI)
+  .then(function mongoSuccess(result) {
+    app.listen(3000);
+  })
+  .catch(function mongoError(error) {
+    console.log(error);
+  });
 
 app.set('view engine', 'ejs');
 
-app.listen(3000);
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+app.get('/add-blog', function addBlogCB(req, res) {
+  const blog = new Blog({
+    title: 'new post',
+    snippet: 'about my new blog',
+    body: 'blog body',
+  });
+
+  blog
+    .save()
+    .then(function saveSuccess(result) {
+      res.send(result);
+    })
+    .catch(function saveError(err) {
+      console.log(err);
+    });
+});
+
 app.get('/', function getHomeCB(req, res) {
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
+});
+
+app.get('/blogs', function getBlogsCB(req, res) {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then(function findSuccess(result) {
+      res.render('index', { title: 'All Blogs', blogs: result });
+    })
+    .catch(function findError(err) {
+      console.log(err);
+    });
 });
 
 app.get('/about', function getAboutCB(req, res) {
