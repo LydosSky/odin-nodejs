e3c59afe4e4be902079680ebf5e4f7c2ea1d52de
Lydos <lydossky@gmail.com>
2025-01-09 20:45:29 +0300

    express controllers

--- express/app.js
+++ express/app.js
@@ -8,6 +8,10 @@ const indexRouter = require('./routes/indexRouter');
 app.use('/authors', authorRouter);
 app.use('/books', bookRouter);
 app.use('/', indexRouter);
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.statusCode || 500).send(err.message);
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
