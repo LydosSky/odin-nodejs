aa5964cb77247019dc8d52d623a615b5db76f201
Lydos <lydossky@gmail.com>
2025-01-09 17:27:43 +0300

    new way of structuring files and folders

--- ninjanode/app.js
+++ ninjanode/app.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const blogRoutes = require("./routes/blogRoutes");
 
 const DBURI =
-  'mongodb+srv://admin:nodepassword@cluster0.houd8.mongodb.net/node?retryWrites=true&w=majority&appName=Cluster0';
+  "mongodb+srv://admin:nodepassword@cluster0.houd8.mongodb.net/node?retryWrites=true&w=majority&appName=Cluster0";
+const app = express();
 
 mongoose
   .connect(DBURI)
@@ -16,22 +16,22 @@ mongoose
     console.log(error);
   });
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', function getHomeCB(req, res) {
-  res.redirect('/blogs');
+app.get("/", function getHomeCB(req, res) {
+  res.redirect("/blogs");
 });
 
-app.get('/about', function getAboutCB(req, res) {
-  res.render('about', { title: 'About' });
+app.get("/about", function getAboutCB(req, res) {
+  res.render("about", { title: "About" });
 });
 
-app.use('/blogs', blogRoutes);
+app.use("/blogs", blogRoutes);
 
 app.use(function notFoundCB(req, res) {
-  res.status(404).render('404', { title: '404' });
+  res.status(404).render("404", { title: "404" });
 });
