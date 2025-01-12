0be92513dde5a893c72f1d7c45cb4b604380e969
Lydos <lydossky@gmail.com>
2025-01-12 12:50:07 +0300

    expanded user model

--- form-and-data-handling/app.js
+++ form-and-data-handling/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('node:path');
+
 const app = express();
 const usersRouter = require('./routes/usersRouter');
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', usersRouter);
 
 const PORT = process.env.PORT || 3000;
--- form-and-data-handling/controllers/usersController.js
+++ form-and-data-handling/controllers/usersController.js
@@ -3,20 +3,43 @@ const { body, validationResult } = require('express-validator');
 
 const alphaErr = 'must only contain letters.';
 const lengthErr = 'must be between 1 and 10 characters.';
-
+const genericErr = 'must be valid';
+const genericReq = 'is required';
 const validateUser = [
   body('firstName')
     .trim()
+    .notEmpty()
+    .withMessage(`First name ${genericReq}`)
     .isAlpha()
     .withMessage(`First name ${alphaErr}`)
     .isLength({ min: 1, max: 10 })
     .withMessage(`First name ${lengthErr}`),
   body('lastName')
     .trim()
+    .notEmpty()
+    .withMessage(`Last name ${genericReq}`)
     .isAlpha()
     .withMessage(`Last name ${alphaErr}`)
     .isLength({ min: 1, max: 10 })
     .withMessage(`Last name ${lengthErr}`),
+  body('email')
+    .trim()
+    .notEmpty()
+    .withMessage(`Email ${genericReq}`)
+    .isEmail()
+    .withMessage(`Email ${genericErr}`)
+    .normalizeEmail(),
+  body('age')
+    .optional({ checkFalsy: true })
+    .isInt({ min: 18, max: 120 })
+    .withMessage(`Age ${genericErr} number between 0 and 120`)
+    .toInt(),
+  body('bio')
+    .optional({ checkFalsy: true })
+    .trim()
+    .escape()
+    .isLength({ min: 0, max: 200 })
+    .withMessage('Bio must be between 0 and 200 character long'),
 ];
 
 exports.usersListGet = (req, res) => {
@@ -43,8 +66,8 @@ exports.usersCreatePost = [
       });
     }
 
-    const { firstName, lastName } = req.body;
-    usersStorage.addUser({ firstName, lastName });
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.addUser({ firstName, lastName, email, age, bio });
     res.redirect('/');
   },
 ];
@@ -70,8 +93,14 @@ exports.usersUpdatePost = [
       });
     }
 
-    const { firstName, lastName } = req.body;
-    usersStorage.updateUser(req.params.id, { firstName, lastName });
+    const { firstName, lastName, email, age, bio } = req.body;
+    usersStorage.updateUser(req.params.id, {
+      firstName,
+      lastName,
+      email,
+      age,
+      bio,
+    });
     res.redirect('/');
   },
 ];
